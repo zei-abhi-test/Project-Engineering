f3661c3e5cd0bb306f37ef46096cbeaeecaf5adb
zei-abhi-test <usetoshiba27@gmail.com>
2026-07-07 21:49:14 +0530

    fix: move secrets to environment variables

--- Milestone 08/lu-8.5-Secrets-and-Environment-Variables/src/middleware/auth.js
+++ Milestone 08/lu-8.5-Secrets-and-Environment-Variables/src/middleware/auth.js
@@ -10,7 +10,7 @@ const jwt = require("jsonwebtoken");
 //      is different (or missing entirely).
 // ============================================================
 
-const JWT_SECRET = "super-secret-key-123";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticate = (req, res, next) => {
   try {
